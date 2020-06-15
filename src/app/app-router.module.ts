7070a5525316d495a355a96d1b09364f8cd7ccfa
Anushk Vijayvargiya <anushk.vijayvargiya.17cse@bml.edu.in>
2020-06-15 19:10:39 +0530

    Additional Components

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,11 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomePageComponent } from './home-page/home-page.component';
-import { RegisterComponent } from './register/register.component'
+import { RegisterComponent } from './register/register.component';
+import { SignInComponent  } from "./sign-in/sign-in.component";
 
 const routes:Routes =[
     {path:'', component:HomePageComponent},
-    {path:'register',component:RegisterComponent}
+    {path:'register',component:RegisterComponent},
+    {path:'login',component:SignInComponent}
 ]
 
 @NgModule({
